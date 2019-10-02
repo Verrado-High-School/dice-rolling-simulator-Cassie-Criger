ea4503b3d9e5d63f76a24c2b4aaaffd481db1d9d
Cassie-Criger <56050487+Cassie-Criger@users.noreply.github.com>
2019-10-02 15:03:06 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -9,7 +9,22 @@ side3 = 0
 side4 = 0
 side5 = 0
 side6 = 0
-rolls = random.random(1,6)
+roll = random.random(1,6)
 
 print("Welcome to Dice Simulator")
-pName = input("How many times do you wanna roll?: ")
+rolls = input("How many times do you wanna roll?: ")
+
+while True:
+
+  if roll == "1":
+    side1 += 1
+  elif roll == "2":
+    side2 += 1
+  elif roll == "3":
+    side3 += 1
+  elif roll == "4":
+    side4 += 1
+  elif roll == "5":
+    side5 += 1
+  else roll == "6":
+    side6 += 1
